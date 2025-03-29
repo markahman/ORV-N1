a691feb4fdceb8b5867ca45a32a48af3abbd3c50
markahman <mark.ahman1@gmail.com>
2025-03-29 20:59:50 +0100

    Implemented aditional helper functions

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 480
 color_tolerance = 1.0
 use_hsv = True
+show_mask = True
+box_size = 50
+box_tolerance = 0.2
+neighbour_filter = 2
+big_box = False
 
 def doloci_barvo_koze(slika: np.ndarray, levo_zgoraj: tuple[int, int], desno_spodaj: tuple[int, int]) -> tuple[np.ndarray, np.ndarray]:
     global color_tolerance, use_hsv
@@ -45,3 +50,41 @@ def prestej_piksle_z_barvo_koze(slika: np.ndarray, barva_koze: tuple[np.ndarray,
     lowerBound, upperBound = barva_koze
     mask = cv.inRange(slika, lowerBound, upperBound)
     return cv.countNonZero(mask)
+
+def chooseSkinArea(cap: cv.VideoCapture) -> tuple[np.ndarray, tuple[int, int], tuple[int, int]]:
+    for _ in range(10):
+        ret, frame = cap.read()
+        if not ret:
+            return None
+    ret, frame = cap.read()
+    if not ret:
+        return None
+    frame = zmanjsaj_sliko(frame, WINDOW_WIDTH, WINDOW_HEIGHT)
+    roi = cv.selectROI(frame, showCrosshair=False)
+    cv.destroyAllWindows()
+    if roi[2] == 0 or roi[3] == 0:
+        return None
+    x0, y0, w, h = roi
+    x1, y1 = x0 + w, y0 + h
+    return (frame, (x0, y0), (x1, y1))
+
+def filterBoxes(boxes: list[list[int]]) -> list[list[int]]:
+    global box_tolerance, neighbour_filter
+    filteredBoxes = [[False for _ in row] for row in boxes]
+    numRows = len(boxes)
+    numCols = len(boxes[0]) if numRows > 0 else 0
+
+    for i, row in enumerate(boxes):
+        for j, numSkinPixels in enumerate(row):
+            if numSkinPixels > box_tolerance * box_size * box_size:
+                neighbors = 0
+                for di in range(-1, 2):
+                    for dj in range(-1, 2):
+                        if di == 0 and dj == 0:
+                            continue
+                        ni, nj = i + di, j + dj
+                        if 0 <= ni < numRows and 0 <= nj < numCols and boxes[ni][nj] > box_tolerance * box_size * box_size:
+                            neighbors += 1
+                if neighbors >= neighbour_filter:
+                    filteredBoxes[i][j] = True
+    return filteredBoxes
